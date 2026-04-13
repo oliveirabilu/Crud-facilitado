341e65e3de7c1627428a56fa11af0436e115c6de
oliveirabilu <carloscesaroliveira13@hotmail.com>
2026-04-13 18:04:00 -0300

    Colocado os atributos para minha Entidade, de forma privada, então preciso abrir os metodos Getter e Setter. Como está usando JPA precisa de um construtor vazio e um com parametros, se estiver usando o Lombok poderia ter colocado as anottations dele para gerar, os metodos e construtores

--- src/main/java/com/examplecrud/crud/entities/Pessoa.java
+++ src/main/java/com/examplecrud/crud/entities/Pessoa.java
@@ -1,9 +1,41 @@
 package com.examplecrud.crud.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "Tb_Pessoa")
 public class Pessoa {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nome;
+    private String idade;
+
+    public Pessoa() {
+    }
+
+    public Pessoa(Long id, String nome, String idade) {
+        this.id = id;
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getIdade() {
+        return idade;
+    }
+    public void setIdade(String idade) {
+        this.idade = idade;
+    }
 }
