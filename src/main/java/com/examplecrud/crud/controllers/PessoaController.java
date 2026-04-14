732fcce4741b121b41177d411a2ca887c3d696fb
oliveirabilu <carloscesaroliveira13@hotmail.com>
2026-04-14 12:07:40 -0300

    Criamos o pacote Controller, e criamos a classe PessoaController com annotation Rest para que retorne Json, e request mapping indicando nosso endpoint de pesquisa

---
+++ src/main/java/com/examplecrud/crud/controllers/PessoaController.java
@@ -0,0 +1,9 @@
+package com.examplecrud.crud.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/pessoas")
+public class PessoaController {
+}
