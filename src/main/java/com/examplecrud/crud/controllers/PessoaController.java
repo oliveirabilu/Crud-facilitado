352195dd6b422003f6ab04691ba24550e00fb191
oliveirabilu <carloscesaroliveira13@hotmail.com>
2026-04-15 10:52:57 -0300

    Nossa busca por endpoint e Verbos Http, Get, Post... e utilizado o Response Entity para retornar codigos de busca/resposta

--- src/main/java/com/examplecrud/crud/controllers/PessoaController.java
+++ src/main/java/com/examplecrud/crud/controllers/PessoaController.java
@@ -1,9 +1,35 @@
 package com.examplecrud.crud.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.examplecrud.crud.entities.Pessoa;
+import com.examplecrud.crud.services.PessoaService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/pessoas")
 public class PessoaController {
+    private final PessoaService pessoaService;
+
+    public PessoaController(PessoaService pessoaService) {
+        this.pessoaService = pessoaService;
+    }
+    @GetMapping
+    public ResponseEntity<List<Pessoa>> buscarPessoa(){
+        return ResponseEntity.ok(pessoaService.listar());
+    }
+    @PostMapping
+    public ResponseEntity<Pessoa> cadastrarPessoa(@RequestBody Pessoa pessoa){
+        return ResponseEntity.status(HttpStatus.CREATED).body(pessoaService.salvar(pessoa));
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> apagar(@PathVariable Long id){
+        return ResponseEntity.noContent().build();
+    }
+    @PutMapping
+    public ResponseEntity<Pessoa> atualizar(@RequestBody Pessoa pessoa){
+        return ResponseEntity.ok().body(pessoa);
+    }
 }
